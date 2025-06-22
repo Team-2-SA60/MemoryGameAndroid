5e4dd50416d041a65f1ab25131e956a3f6cfff22
im-ksc <chan_kin_seng@hotmail.com>
2025-06-23 00:04:07 +0800

    chore: sync with latest changes from main, removed magic number check from detekt

--- app/src/main/java/com/example/memorygameteam2/playactivity/CardAdapter.kt
+++ app/src/main/java/com/example/memorygameteam2/playactivity/CardAdapter.kt
@@ -9,9 +9,8 @@ import com.example.memorygameteam2.R
 
 class CardAdapter(
     private val cards: List<Card>,
-    private val onClick: (position: Int) -> Unit
+    private val onClick: (position: Int) -> Unit,
 ) : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
-
     inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val imgFront: ImageView = v.findViewById(R.id.imgFront)
         val imgBack: ImageView = v.findViewById(R.id.imgBack)
@@ -19,32 +18,32 @@ class CardAdapter(
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
-        viewType: Int
+        viewType: Int,
     ) = ViewHolder(
         LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_card, parent, false))
+            .inflate(R.layout.item_card, parent, false),
+    )
 
     override fun onBindViewHolder(
         holder: ViewHolder,
-        pos: Int
+        pos: Int,
     ) {
         val card = cards[pos]
 
         // not face up & not matched: show back
-        holder.imgBack.visibility = if(!card.isFaceUp && !card.isMatched) View.VISIBLE else View.GONE
+        holder.imgBack.visibility = if (!card.isFaceUp && !card.isMatched) View.VISIBLE else View.GONE
         // face up or matched: show front
-        holder.imgFront.visibility = if(card.isFaceUp || card.isMatched) View.VISIBLE else View.GONE
-        if(card.isFaceUp || card.isMatched){
+        holder.imgFront.visibility = if (card.isFaceUp || card.isMatched) View.VISIBLE else View.GONE
+        if (card.isFaceUp || card.isMatched) {
             holder.imgFront.setImageResource(card.imageRes)
         }
 
-        holder.itemView.setOnClickListener{
-            if(!card.isFaceUp && !card.isMatched) {
+        holder.itemView.setOnClickListener {
+            if (!card.isFaceUp && !card.isMatched) {
                 onClick(pos)
             }
         }
     }
 
     override fun getItemCount(): Int = cards.size
-
 }
