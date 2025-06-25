ea6c24d8cff38eddbe72aef2fd074226162779f7
kinsengchan <chan_kin_seng@hotmail.com>
2025-06-25 14:32:53 +0800

    feat: Leaderboard animation and added 1 more gamebg

--- app/src/main/java/com/example/memorygameteam2/LeaderboardActivity.kt
+++ app/src/main/java/com/example/memorygameteam2/LeaderboardActivity.kt
@@ -1,10 +1,13 @@
 package com.example.memorygameteam2
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.memorygameteam2.databinding.ActivityLeaderboardBinding
 import com.example.memorygameteam2.leaderboard.LeaderboardAdapter
@@ -40,6 +43,10 @@ class LeaderboardActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView() {
+        val animation: LayoutAnimationController? =
+            AnimationUtils.loadLayoutAnimation(this, R.anim.layout_animation_fall_down)
+        binding.leaderboardRecyclerView.layoutAnimation = animation
+        binding.leaderboardRecyclerView.itemAnimator = DefaultItemAnimator()
         binding.leaderboardRecyclerView.layoutManager = LinearLayoutManager(this)
         leaderboardAdapter = LeaderboardAdapter(rankingList)
         binding.leaderboardRecyclerView.adapter = leaderboardAdapter
@@ -55,26 +62,47 @@ class LeaderboardActivity : AppCompatActivity() {
             todayBtn.setOnClickListener {
                 SoundManager.playButtonClick(this@LeaderboardActivity)
                 rankingList = RankDAO.getRankList("today")
-                leaderboardAdapter.updateData(rankingList)
+                updateRankingList(rankingList)
             }
 
             last7DaysBtn.setOnClickListener {
                 SoundManager.playButtonClick(this@LeaderboardActivity)
                 rankingList = RankDAO.getRankList("last7")
-                leaderboardAdapter.updateData(rankingList)
+                updateRankingList(rankingList)
             }
 
             last30DaysBtn.setOnClickListener {
                 SoundManager.playButtonClick(this@LeaderboardActivity)
                 rankingList = RankDAO.getRankList("last30")
-                leaderboardAdapter.updateData(rankingList)
+                updateRankingList(rankingList)
             }
 
             allTimeBtn.setOnClickListener {
                 SoundManager.playButtonClick(this@LeaderboardActivity)
                 rankingList = RankDAO.getRankList("allTime")
-                leaderboardAdapter.updateData(rankingList)
+                updateRankingList(rankingList)
             }
         }
+    }
+
+    private fun updateRankingList(newRankingList: List<Rank>) {
+        // Fade out current list (VISIBLE -> INVISIBLE)
+        binding.leaderboardRecyclerView.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .withEndAction {
+                // Update old Ranking list in adapter
+                leaderboardAdapter.updateData(newRankingList)
+
+                // start the falldown animation
+                binding.leaderboardRecyclerView.scheduleLayoutAnimation()
+
+                // fade in the list (INVISIBLE -> VISIBLE)
+                binding.leaderboardRecyclerView.animate()
+                    .alpha(1f)
+                    .setDuration(300)
+                    .start()
+            }
+            .start()
     }
 }
