f518e9dc57afa0e522dfdb8e121cea45f7505455
adriantlh <adrian.tan.li.huat@gmail.com>
2025-07-02 14:42:46 +0800

    Added saving USERID and User name and it will now properly send over the correct userId upon game end

--- app/src/main/java/com/example/memorygameteam2/PrefsHelper.kt
+++ app/src/main/java/com/example/memorygameteam2/PrefsHelper.kt
@@ -5,15 +5,17 @@ import android.content.Context
 class PrefsHelper(context: Context) {
     private val sharedPref = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
 
-    fun saveUser(username: String, isPremium: Boolean) {
+    fun saveUser(username: String, isPremium: Boolean, id: String) {
         sharedPref.edit().apply {
             putString("username", username)
             putBoolean("is_premium", isPremium)
+            putString("id", id)
             apply()
         }
     }
 
     fun getUsername(): String? = sharedPref.getString("username", null)
+    fun getUserID(): String? = sharedPref.getString("id", null)
     fun isPremium(): Boolean = sharedPref.getBoolean("is_premium", false)
     fun clearUser() {
         sharedPref.edit().apply {
